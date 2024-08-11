a7c8be416b2a9f13a7c36fa143270c4fe333fb00
Harsh Rana <harshrana8454@gmail.com>
2024-08-11 15:20:31 +0530

    add

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -8,7 +8,7 @@ const Qualification = () => {
     }
   return (
     <section className="qualification section">
-        <h2 className="section__title">Qualification</h2>
+        <h2 className="section__title">Qualification & Experiance</h2>
         <span className="section__subtitle">My personal journey</span>
         <div className="qualification__container container">
             <div className="qualification__tabs">
